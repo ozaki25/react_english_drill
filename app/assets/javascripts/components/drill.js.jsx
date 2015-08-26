f89afec1ef92cfeae852b8dcae50d2cf69379544
ozaki <fs65ugumori@yahoo.co.jp>
2015-08-26 15:09:11 +0900

    直書きで再現

--- app/assets/javascripts/components/drill.js.jsx
+++ app/assets/javascripts/components/drill.js.jsx
@@ -6,6 +6,8 @@ this.EnglishDrill = React.createClass({
     <span className="title">
       <a href="/">English Dril</a>
     </span>
+    <hr />
+    <ProgressInfo />
     <Drill />
   </div>
 </body>
@@ -13,7 +15,7 @@ this.EnglishDrill = React.createClass({
   }
 });
 
-this.Drill = React.createClass({
+this.ProgressInfo = React.createClass({
   render: function() {
     return(
 <div className="row">
@@ -21,22 +23,46 @@ this.Drill = React.createClass({
     <h2>SECTION 17</h2>
     <span className="number">No.86</span>
   </div>
-  <div className="col-md-2 counter-block">
-    <div className="counter-text">You Answered</div>
-    <div className="counter-number">3</div>
-    <div className="counter-text">times.</div>
+  <div className="col-md-2">
+    <div className="counter-block">
+      <div className="counter-text">You Answered</div>
+      <div className="counter-number">3</div>
+      <div className="counter-text">times.</div>
+    </div>
   </div>
-  <div className="col-md-2 counter-block">
-    <div className="counter-text">You Cleared</div>
-    <div className="counter-number">0</div>
-    <div className="counter-text">times.</div>
+  <div className="col-md-2">
+    <div className="counter-block">
+      <div className="counter-text">You Cleared</div>
+      <div className="counter-number">0</div>
+      <div className="counter-text">times.</div>
+    </div>
   </div>
-  <div className="col-md-2 counter-block lastfive">
-    <div className="counter-text">Last</div>
-    <div className="counter-number">5</div>
-    <div className="counter-text">to clear this section.</div>
+  <div className="col-md-2">
+    <div className="counter-block lastfive">
+      <div className="counter-text">Last</div>
+      <div className="counter-number">5</div>
+      <div className="counter-text">to clear this section.</div>
+    </div>
   </div>
 </div>
+    )
+  }
+});
+
+this.Drill = React.createClass({
+  render: function() {
+    return(
+<form action="#" method="post">
+  <div className="sentence-block">
+    <label>Japanese</label>
+    <div className="japanese">あなたの考えは理論上はすばらしそうだけれども、実際には機能しないと思います。</div>
+  </div>
+  <div className="sentence-block form-group">
+    <label>English</label>
+    <input id="inTxi" type="text" name="input" className="form-control" />
+  </div>
+  <input type="submit" value="SEND" className="btn btn-primary btn-large" />
+</form>
     )
   }
 });
