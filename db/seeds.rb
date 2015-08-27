8c10cdba113b9a203c48d9d8e90ff2bf751a459d
ozaki25 <fs65ugumori@yahoo.co.jp>
2015-08-28 08:54:48 +0900

    seedをちょっと作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Drill.create(exeid: 0, section_no: 0, japanese: "これはペンです。", english: "This is a pen.")
+Drill.create(exeid: 1, section_no: 0, japanese: "個人の意思は尊重しなければならない。", english: "We must respect the will of the individual.")
+Drill.create(exeid: 2, section_no: 0, japanese: "気楽にいけよ。大丈夫、すべてうまくいくさ。", english: "Take it easy. I can assure you that everything will turn out fine.")
+Drill.create(exeid: 3, section_no: 0, japanese: "悲観的な人生観を捨てましょう。前向きな態度を常に持ち続けよう。", english: "Let go of your negative outlook on life. Always maintain a positive attitude.")
+Drill.create(exeid: 4, section_no: 0, japanese: "生まれた国、性別、信条に関係無く、誰に対しても公平でなくてはならない。", english: "You should be fair to everyone regardless of national origin, gender, or creed.")
+Drill.create(exeid: 5, section_no: 1, japanese: "平等は憲法で守られている。", english: "Equality is guaranteed by the Constitution.")
+Drill.create(exeid: 6, section_no: 1, japanese: "彼は柱に寄り掛かって、自由の女神像をじっとみつめた。", english: "He leaned against the pillar and gazed at the Statue of Liberty.")
+Drill.create(exeid: 7, section_no: 1, japanese: "香水のほのかな香りを漂わせながら、一人の女性が僕の前を通り過ぎた。前の彼女のことを思い出してしまった。", english: "A woman passed by me giving off a subtle scent of perfume. It reminded me of my ex-girlfriend.")
+Drill.create(exeid: 8, section_no: 1, japanese: "納豆はにおいはひどいけれど味は最高。", english: "\"Natto\" smells awful but tastes terrific.")
+Drill.create(exeid: 9, section_no: 1, japanese: "「汗でびしょぬれだよ。」「来ないで！臭いわ。シャワーを浴びて。」", english: "\"I'm soaked with sweat.\" \"Stand back! You stink. Take a shower.\"")
+Drill.create(exeid: 10, section_no: 2, japanese: "ボブはひどく取り乱していて、現実と虚構の区別がほとんどできなかった。", english: "Bob was so beside himself that he could scarcely tell fact from fiction.")
+Drill.create(exeid: 11, section_no: 2, japanese: "まもなく出版になる彼の小説は、散文に彼の詩の才能を融合させたものだ。", english: "His new novel, which combines prose with his gift for poetry, is going to be published.")
+Drill.create(exeid: 12, section_no: 2, japanese: "来月、その百科事典の最新版が出る。", english: "An up-to-date edition of the encyclopedia will come out next month.")
+Drill.create(exeid: 13, section_no: 2, japanese: "山田さんがその心ひかれるおとぎ話をやさしい日本語に翻訳した。", english: "Ms. Yamada translated the fascinating fairy tale into plain Japanese.")
+Drill.create(exeid: 14, section_no: 2, japanese: "以下の一説は有名な寓話からの引用です。", english: "The following passage is quoted from a well-known fable.")
+Drill.create(exeid: 15, section_no: 3, japanese: "「現代文学に詳しいですか？」「ほとんど知りません。」", english: "\"Are you familiar with contemporary literature?\" \"I know next to nothing about it.\"")
+Drill.create(exeid: 16, section_no: 3, japanese: "時々、\"curve\"と\"carve\"を混同してしまう。", english: "At times I confuse \"curve\" with \"carve\".")
+Drill.create(exeid: 17, section_no: 3, japanese: "恥ずかしがらないで。あなたの発音はだいたい合っています。", english: "Don't be shy. Your pronunciation is more or less correct.")
+Drill.create(exeid: 18, section_no: 3, japanese: "この記事には、語彙を本気で増やしたいと思う人々に役立つ情報が含まれています。", english: "This article contains tips for those who are eager to increase their vocabulary.")
+Drill.create(exeid: 19, section_no: 3, japanese: "彼の最新の作品が市庁舎で展示されているの。言葉にできないほど素敵よ。", english: "His latest works are on display at the city hall. They are fabulous beyond description.")
+
+
+
+
+
+
